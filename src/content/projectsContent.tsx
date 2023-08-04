0b56cf14f288bc7fbc72b9f49e19723f469a3d5b
Geoffrey Doak <gdoak@pop-os.localdomain>
2023-08-04 16:26:23 +1000

    fixed incorrect link

--- src/content/projectsContent.tsx
+++ src/content/projectsContent.tsx
@@ -12,7 +12,7 @@ const projectsContent = [
     {
         title: 'Back Massager',
         content: 'A 3D printable model for a back massager written in OpenSCAD. Using a parametric design, you can adjust it for the perfect fit',
-        url: 'https://github.com/Geoffdoak'
+        url: 'https://github.com/Geoffdoak/backmassager'
     },
 ]
 
